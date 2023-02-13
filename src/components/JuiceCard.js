5c7390668487a03c418d28d4881179b3567b9d7c
braincee <kwesi938@gmail.com>
2023-02-13 16:44:06 +0000

    add delete function and icon to Juice Card

--- src/components/JuiceCard.js
+++ src/components/JuiceCard.js
@@ -1,15 +1,37 @@
 import React from 'react'
 import { HiPencil } from 'react-icons/hi';
+import { FcEmptyTrash } from 'react-icons/fc';
 import { Link } from 'react-router-dom'
+import supabase from '../config/supabaseClient';
 
-const JuiceCard = ({ juice }) => {
+const JuiceCard = ({ juice, onDelete }) => {
+
+  const handleDelete = async () => {
+
+    const { data, error } = await supabase
+    .from("Juice")
+    .delete()
+    .eq("id", juice.id)
+    .select()
+
+    if (error) {
+      console.log(error)
+    }
+
+    if (data) {
+      onDelete(juice.id)
+    }
+  }
+
+
   return (
     <div className='juice-card'>
         <h3>{juice.title}</h3>
         <p>{juice.method}</p>
         <div className='rating'>{juice.ratings}</div>
-        <div className='buttons pencil'>
-            <Link to={'/' + juice.id}><HiPencil /></Link>
+        <div className='buttons'>
+          <Link to={'/' + juice.id} className="pencil"><HiPencil size={20}/></Link>
+          <FcEmptyTrash size={20} className="trash" onClick={handleDelete} />
         </div>
     </div>
   )
